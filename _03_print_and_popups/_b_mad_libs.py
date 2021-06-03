393bb03385d70cda8633408e19ee5966765d6cdb
root <root@root.com>
2021-06-03 00:02:18 +0000

    was working on world domination.

--- _03_print_and_popups/_b_mad_libs.py
+++ _03_print_and_popups/_b_mad_libs.py
@@ -3,21 +3,23 @@ from tkinter import messagebox, simpledialog, Tk
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
 if __name__ == '__main__':
     # Make a new window variable, window = Tk()
-
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-
+    window.withdraw()
     # Put this sentence in a pop-up message box:
     # "If you find yourself having to cross a piranha-infested river, here's how to do it..."
-
+    messagebox.showinfo(title='',message="If you find yourself having to cross a potato-infested, here is how to do it...")
     # Get the player to enter an adjective
-
+    adj = simpledialog.askstring(title='adj', prompt="Enter an adjective")
     # Get the player to enter a type of liquid
-
+    liquid = simpledialog.askstring(title='liquid', prompt="Enter an liquid")
     # Get the player to enter a body part
-
+    bodypart = simpledialog.askstring(title='body part', prompt="Enter an body part")
     # Get the player to enter a verb
-
+    verb = simpledialog.askstring(title='verb', prompt="Enter an verb")
     # Get the player to enter a place
+    place = simpledialog.askstring(title='place', prompt="Enter an place")
+
 
     # The story below has has been written as a group of Strings joined
     # together by + signs. The story contains place holders, indicated
@@ -27,11 +29,11 @@ if __name__ == '__main__':
     #       other parts of the story.
 
     story = (
-        "Piranhas are more [**adjective**] during the day, so cross the river at\n"
-        "night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"
-        "likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"
+        "Piranhas are more " + adj + " during the day, so cross the river at\n"
+        "night. Piranhas are attracted to fresh  " + liquid + " and will most\n"
+        "likely take a bite out of your " + bodypart + " if you " + verb + ". Whatever\n"
         "you do, if you have an open wound, try to find another way to get "
-        "back to the [**place**]. Good luck!"
+        "back to the " + place + ". Good luck!"
     )
 
     # Make a pop-up that contains the final story. The \n escape characters add
@@ -40,7 +42,7 @@ if __name__ == '__main__':
 
     # If you want to write your own Madlib story, just change the story variable
     # and ask the player different questions.
-
+    messagebox.showinfo(title="mad libs", message=story)
     # Run the window's .mainloop() method
 
     pass
--- _03_print_and_popups/_c_world_domination.py
+++ _03_print_and_popups/_c_world_domination.py
@@ -3,13 +3,13 @@ from tkinter import messagebox, simpledialog, Tk
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
     
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    window.withdraw()
     # 1. Ask the user if they know how to write code.
-    
+    askuser = simpledialog.askstring(title='Q&A', prompt="do you know how to wrote code?")
     # 2. If they say "yes", tell them they will rule the world in a message box pop-up.
-
+    if
     # 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
     
     # Run the window's .mainloop() method
