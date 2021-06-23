a06244b55241926bea5bfe52336c8fac3aee9354
andrewcarta <andrewcarta08@gmail.com>
2021-06-23 23:59:59 +0000

    do #10-11

--- _04_amazing_games/_a_terminator_cat.py
+++ _04_amazing_games/_a_terminator_cat.py
@@ -45,7 +45,7 @@ def key_pressed():
 
     # 10. Increment the x and y variables of the 2 eye variables by 5:
     #     left_eye.x += 5
-    
+    left_eye.x += 5
     # 11. Call the .draw() method for both eye variables.
 
 
@@ -61,31 +61,34 @@ if __name__ == '__main__':
     
     # 2. Call the set_background() function with your variable inside of the parenthesis
     #    for example, set_background(bg_image)
+    set_background('bigEyedCat.gif')
 
     # 3. Make a new turtle
-
+    potato=turtle.Turtle()
     # 4. Set the turtle color and pen color to red (or any color you want)
     #    using .color('red', 'red')
-
+    potato.color('red', "red")
     # 5. Set the turtle width to 0 so no outlines are drawn
-
+    potato.width(0)
     # 6. Set the turtle speed to 0 (fastest)
-
+    potato.speed(0)
     # 7. Run the program and click on one of the cat's eyes. 
     #    The x,y position of the eye will be printed at the bottom of your
     #    processing window.
     #    Variables for x and y have been created at the top of your sketch, 
     #    now you can set them equal to the values you just found. Watch for
     #    negative signs!
-    
+    eyeL=('32', '28')
+    eyeR=('47','14')
     # 8. After you've found the x and y for the eyes create 2 eye variables
     #    and initialize them:
     #    left_eye  = Eye(eye=my_turtle, x=-34, y=11, radius=30)
     #    right_eye = Eye(eye=my_turtle, x=40, y=-5, radius=30)
-
+    left_eye = Eye(eye=potato, x=-32, y=28, radius=30)
+    right_eye = Eye(eye=potato, x=47, y=-14, radius=30)
     # 9. Call the .draw() method on BOTH eye variables
-
-
+    left_eye.draw()
+    right_eye.draw()
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
     window.onkeypress(key_pressed, 'space')
